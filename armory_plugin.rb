11654f9d51b6cc09da6cb79d8eac3d4b684a3a57
Raine Virta <rainevirta@Macintosh.local>
2008-05-21 22:42:25 +0300

    -

--- armory_plugin.rb
+++ armory_plugin.rb
@@ -2,10 +2,10 @@ require '~/armory'
 
 class ArmoryPlugin < Plugin
   def search(m, params)
-    pp "asd"
-    pp Armory.new(:us).search(:character, "Jakuten")
+    pp params
+    #pp Armory.new(:us).search(:character, "Jakuten")
   end
 end
 
 plugin = ArmoryPlugin.new
-plugin.map "s *str", :action => 'search'
+plugin.map ":region *str", :action => 'search', :requirements => { :region => %r|eu|us| }
