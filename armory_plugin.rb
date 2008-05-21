0543c33b0bd6b128fc716874596f2f3d774e2e3f
Raine Virta <rainevirta@Macintosh.local>
2008-05-21 22:23:55 +0300

    -

--- armory_plugin.rb
+++ armory_plugin.rb
@@ -1,7 +1,11 @@
 require '~/armory'
 
 class ArmoryPlugin < Plugin
-  
+  def search(m, params)
+    m.reply "foo"
+    pp Armory.new(:us).search("Jakuten")
+  end
 end
 
 plugin = ArmoryPlugin.new
+plugin.map "s *str", :action => 'search'
